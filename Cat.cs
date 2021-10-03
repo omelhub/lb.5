9ed0161c9b719a433bfef5174af43cdc67b60f11
justd <justd@LAPTOP-N5CUAFJ4>
2021-10-03 14:05:59 +0700

    Добавьте файлы проекта.

---
+++ Cat.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лб._5
+{
+    class Cat
+    {
+        private string _name;
+        public string Name
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(_name))
+                {
+                    _name = value;
+                }
+            }
+            get { return _name; }
+        }
+
+        private uint _age;
+        public uint Age
+        {
+            get { return _age; }
+        }
+        public Cat(uint Age)
+        {
+            _age = Age;
+        }
+        
+        public int Health { set; get; }
+
+        public string Color
+        {
+            get
+            {
+                if (Health < 0)
+                    return "white";
+                if (Health > 0)
+                    return "green";
+                else
+                    return "red";
+            }
+        }
+
+        public void Feed(int foodCount)
+        {
+            Health += foodCount;
+        }
+
+        public void Punish(int punch)
+        {
+            Health -= punch;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лб._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.Write("Введите имя кота: ");
+            Cat cat = new Cat( (uint)rnd.Next(1, 20)) 
+            {
+                Name = Console.ReadLine()
+            };
+            Console.WriteLine($"Возраст: {cat.Age}");
+            cat.Health = rnd.Next(1,10);
+            Console.WriteLine($"Здоровье: {cat.Health}");
+            while (cat.Health > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(cat.Health);
+                Console.ResetColor();
+                Console.WriteLine("Введите команду ''Feed'' или ''Punish'': ");
+                string str = Console.ReadLine();
+                if (str == "Feed")
+                {
+                    Console.WriteLine("Введите значение foodCount: ");
+                    cat.Feed(Convert.ToInt32(Console.ReadLine()));
+                }
+                else if (str == "Punish")
+                {
+                    Console.WriteLine("Введите значение punch: ");
+                    cat.Punish(Convert.ToInt32(Console.ReadLine()));
+                }
+                if (str == "Age")
+                {
+                    Console.Write("Возраст: ");
+                    Console.WriteLine(cat.Age);
+                }
+            }
+
+            if (cat.Health == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(cat.Health);
+                Console.ResetColor();
+            }
+            else
+                Console.WriteLine(cat.Health);
+            
+            Console.WriteLine("GAME OVER");
+            Console.ReadKey();
+        }
+    }
+}
+
